a538207d0459f3404b874dbec5db0b41e4d1d7f4
BritDua <68259529+BritDua@users.noreply.github.com>
2023-08-22 12:55:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
 terraform {
-  backend "remote" {
-    organization = "devops-directive"
-
-    workspaces {
-      name = "devops-directive-terraform-course"
-    }
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -17,4 +9,9 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  instance_type = "t2.micro"
 }
